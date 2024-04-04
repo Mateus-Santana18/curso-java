67f26ff8aaf3420bbdce7094c7c64a478be2fc4d
MATEUS SANTANA DE OLIVEIRA <mateus_oliveira8@estudante.sesisenai.org.br>
2024-04-04 16:27:45 -0300

    Adicionado pasta da segunda aula

---
+++ segundaAula/src/Main.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        JOptionPane.showMessageDialog(null,"Hello World!!");
+//        int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade:"));
+//        System.out.println(idade);
+
+        System.out.println(JOptionPane.showConfirmDialog(null,"Tem Certeza ?"));
+        System.out.println(JOptionPane.showConfirmDialog(null,"Tem Certeza ?"));
+        System.out.println(JOptionPane.showConfirmDialog(null,"Tem Certeza ?"));
+
+//        Scanner teclado = new Scanner(System.in);
+//        System.out.print("Nome do Aluno: ");
+//        String nome = teclado.nextLine();
+//        System.out.print("Primeira Nota: ");
+//        float notaUm = teclado.nextFloat();
+//        System.out.print("Segunda Nota: ");
+//        float notaDois = teclado.nextFloat();
+//        System.out.print("Terceira Nota: ");
+//        float notaTres = teclado.nextFloat();
+//        float media = (notaUm + notaDois + notaTres) / 3;
+//        System.out.println("====================");
+//        if(media >= 7){
+//            System.out.printf("Aprovado!! A média de %s é %.2f \n", nome,media);
+//        }else if (media < 7 && media >= 5) {
+//            System.out.printf("Recuperação!! A média de %s é %.2f \n", nome,media);
+//        }else{
+//            System.out.printf("Reprovado!! A média de %s é %.2f \n", nome,media);
+//        };
+//        System.out.println("====================");
+    }
+}
