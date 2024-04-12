2ae634182fb8e5438a173c905ca38c4b66ef6498
Mateus-Santana18 <mateus.teteu897@gmail.com>
2024-04-12 13:16:49 -0300

    exercicioPronto

--- exercicioArray/src/Main.java
+++ exercicioArray/src/Main.java
@@ -4,34 +4,34 @@ public class Main {
     public static void main(String[] args) {
         int pergunta = Integer.parseInt(JOptionPane.showInputDialog("Quantas pesquisas deseja fazer ?"));
         float[] arrayPesquisa = new float[pergunta];
+        float[] arrayPesquisaAcrescimo = new float[pergunta];
         for (int i = 0; i < pergunta; i++){
             arrayPesquisa[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite o salário: "));
-        };
-        float maior = -1, salarioAcrescimo = 0, salarioTotal = 0;
-
+        }
+        float maior = -1;
+        float   maiorAcrescimo = -1;
+        for(int i = 0; i < arrayPesquisaAcrescimo.length; i++) {
+            arrayPesquisaAcrescimo[i] = arrayPesquisa[i] * 1.10f;
+        }
         for (float m : arrayPesquisa){
             if (maior < m){
                 maior = m;
             }
         }
-
-        for (float acr : arrayPesquisa){
-            if (acr <= 2500){
-                acr *= 1.10f;
-                salarioAcrescimo = acr;
+        for (float m : arrayPesquisaAcrescimo){
+            if (maiorAcrescimo < m){
+                maiorAcrescimo = m;
             }
         }
-        for (float salario : arrayPesquisa){
-            if (salario <= 2500 ){
-                salarioTotal = salario;
-                System.out.println("O salário é de: " + salario + " Com o acréscimo dos 10% fica um total de: " + salarioAcrescimo);
+        for(int i = 0; i < arrayPesquisa.length; i++) {
+            float value = arrayPesquisa[i];
+            if (value <= 2500 ){
+                System.out.println("O salário é de: " + value + " Com o acréscimo dos 10% fica um total de: " + arrayPesquisaAcrescimo[i]);
             }
         }
-        if (salarioTotal > maior){
-            maior = salarioTotal;
+        System.out.println("O maior salário dentre as pequisas é: " + maior);
+        if (maiorAcrescimo <= 2500) {
+            System.out.println("O maior salário com acrescimo dentre as pequisas é: " + maiorAcrescimo);
         }
-
-        System.out.println("O maior salário é: " + maior);
-
     }
 }
